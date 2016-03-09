6a993c9fb2f21bbdc8761dd399f1d4ca4406d47d
hcbviolet <hcbviolet@gmail.com>
2016-03-08 17:42:49 -0800

    Fix registered_app duplicate URL error message

--- app/controllers/registered_apps_controller.rb
+++ app/controllers/registered_apps_controller.rb
@@ -19,7 +19,8 @@ class RegisteredAppsController < ApplicationController
     if @registered_app.save
       redirect_to registered_apps_path
     else
-      flash[:alert] = "There was a problem with registration. Please try again."
+      flash[:alert] = "There was a problem saving your application. Please make sure you are regsitering a unique URL."
+      redirect_to registered_apps_path
     end
   end
 
